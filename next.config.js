ac209b4874f57d8b7bb443b8089a25c9178c402c
Jacky Cheung <cheungkwingho@gmail.com>
2023-05-14 11:23:14 +0800

    change redirect way

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/blog/page",
+        destination: "/blog/page/1",
+        permanent: true,
+      },
+      {
+        source: "/blog",
+        destination: "/blog/page/1",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
