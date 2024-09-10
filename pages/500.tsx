438fb2990d2afc29c0b2196dcdc19d8f8ba4a429
Jacky Cheung <hello@jackycheung.dev>
2024-09-11 00:27:59 +0800

    style: change 500 page css style

--- pages/500.tsx
+++ pages/500.tsx
@@ -6,7 +6,7 @@ export default function Custom500() {
       <section className="bg-gray-900 h-screen">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
           <div className="mx-auto max-w-screen-sm text-center">
-            <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-purple-900">
+            <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-[#9333ea]">
               500
             </h1>
             <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-white">
