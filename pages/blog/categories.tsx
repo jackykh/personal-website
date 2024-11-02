c5b0826b71a32bbbef78e61c4398c54d177f4998
Jacky Cheung <cheungkwingho@gmail.com>
2024-11-02 17:21:13 +0800

    feat: add categories list page

---
+++ pages/blog/categories.tsx
@@ -0,0 +1,104 @@
+import Head from "next/head";
+import { GetStaticProps } from "next";
+import Navigation from "@/Components/uiComponents/Navigation";
+import Footer from "@/Components/Footer";
+import Link from "next/link";
+import client from "@/lib/apollo-client";
+import { gql } from "@apollo/client";
+import Giscus from "@giscus/react";
+
+const categoriesList = (props: {
+  categories: Array<{
+    name: string;
+    id: number;
+  }>;
+}) => {
+  const { categories } = props;
+  return (
+    <>
+      <Head>
+        <title>Categories - Jacky&apos;s Blog</title>
+        <meta name="description" content="Categories - Jacky's Blog" />
+      </Head>
+      <Navigation />
+      <main className="py-24 px-8 flex flex-col items-center">
+        <h1 className="text-4xl pb-4">Categories List</h1>
+        <div className="flex flex-col w-[60rem] max-w-full items-center">
+          <div className="pt-8 pb-32 max-w-[40rem] flex gap-y-2 gap-x-4 flex-wrap">
+            {categories.map((category) => (
+              <Link
+                href={`/blog/category/${category.id}/page/1`}
+                key={category.name}
+              >
+                <span className="border-b border-solid border-b-black text-xl">
+                  {category.name}
+                </span>
+              </Link>
+            ))}
+          </div>
+          <div className="w-full">
+            <Giscus
+              id="comments"
+              repo="jackykh/blog-comments"
+              repoId="R_kgDOM0EbrQ"
+              category="Announcements"
+              categoryId="DIC_kwDOM0Ebrc4CinA8"
+              mapping="pathname"
+              reactionsEnabled="1"
+              emitMetadata="0"
+              inputPosition="top"
+              theme="light"
+              lang="en"
+              loading="lazy"
+            />
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const CATEGORIES = gql`
+    query GetCategories($start: Int!, $limit: Int!) {
+      categories(pagination: { start: $start, limit: $limit }) {
+        data {
+          id: id
+          attributes {
+            name
+          }
+        }
+      }
+    }
+  `;
+  const { data } = await client.query({
+    query: CATEGORIES,
+    variables: {
+      start: 0,
+      limit: 50,
+    },
+  });
+
+  interface categoriesData {
+    categories: {
+      data: Array<{
+        id: string;
+        attributes: {
+          name: string;
+        };
+      }>;
+    };
+  }
+  const categoriesData = data as categoriesData;
+
+  return {
+    props: {
+      categories: categoriesData.categories.data.map((data) => {
+        return { id: data.id, name: data.attributes.name };
+      }),
+    },
+  };
+};
+
+export default categoriesList;
