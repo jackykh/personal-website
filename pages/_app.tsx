17035739632dd0b831576e3252e660cb449cbc3b
Jacky Cheung <cheungkwingho@gmail.com>
2024-11-09 11:45:55 +0800

    update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,7 +48,23 @@ export default function App({ Component, pageProps, router }: AppProps) {
           name="description"
           content="I am Jacky Cheung, A self-taught Frontend Web Developer. "
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <ApolloProvider client={client}>
