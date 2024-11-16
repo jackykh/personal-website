670cc175d8c10965785d256357eefe8643d19976
Jacky Cheung <cheungkwingho@gmail.com>
2024-11-16 22:01:31 +0800

    limit tracker to only run on jackycheung.dev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -75,6 +75,7 @@ export default function App({ Component, pageProps, router }: AppProps) {
       <Script
         src="https://cloud.umami.is/script.js"
         data-website-id={`${process.env.NEXT_PUBLIC_UMAMI_ID}`}
+        data-domains="jackycheung.dev"
       />
     </>
   );
