7beeae1f1d53cbdb9dbd85f2f2056f88aa60b015
Jacky Cheung <cheungkwingho@gmail.com>
2025-01-01 05:13:23 +0800

    adjust post summary bottom position

--- pages/blog/post/[postId].tsx
+++ pages/blog/post/[postId].tsx
@@ -84,46 +84,49 @@ const Post = (props: postProps) => {
                 ))}
               </span>
             </div>
-
-            {!summary && (
-              <Button
-                borderRadius="1.75rem"
-                className="bg-white text-black border-neutral-200"
-                onClick={generateSummary}
-              >
-                {loading ? (
-                  <div className="flex">
-                    <svg
-                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-purple-900"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        stroke-width="4"
-                      ></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      ></path>
-                    </svg>
-                    加載中
-                  </div>
-                ) : (
-                  <span>試試 AI 摘要✨</span>
-                )}
-              </Button>
-            )}
           </div>
           <div className="py-4 mb-12">
             <h1 className="text-3xl font-semibold mb-4">{props.title}</h1>
-            <Summary content={summary} />
+            {summary ? (
+              <Summary content={summary} />
+            ) : (
+              <div className="mb-4">
+                <Button
+                  borderRadius="1.75rem"
+                  className="bg-white text-black border-neutral-200"
+                  onClick={generateSummary}
+                >
+                  {loading ? (
+                    <div className="flex">
+                      <svg
+                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-purple-900"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          stroke-width="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                      加載中
+                    </div>
+                  ) : (
+                    <span>試試 AI 摘要✨</span>
+                  )}
+                </Button>
+              </div>
+            )}
+
             <ReactMarkdown
               remarkPlugins={[remarkGfm]}
               rehypePlugins={[rehypeRaw as any]}
