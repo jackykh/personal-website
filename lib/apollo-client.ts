48cde5358de6895d98e12a0dfe3afe6b0ed37332
Jacky Cheung <cheungkwingho@gmail.com>
2024-12-31 16:43:53 +0800

    feat: add ai summary api

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,9 +1,14 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 const STRAPI_URI = process.env.NEXT_PUBLIC_STRAPI_URL;
+
+const httpLink = createHttpLink({
+  uri: STRAPI_URI,
+});
+
 const client = new ApolloClient({
-  link: createHttpLink({
-    uri: STRAPI_URI,
-  }),
+  link: httpLink,
   cache: new InMemoryCache(),
   defaultOptions: {
     query: {
@@ -12,4 +17,25 @@ const client = new ApolloClient({
   },
 });
 
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: `bearer ${process.env.BLOG_APIKEY}`,
+    },
+  };
+});
+
+const blogUpdatingClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    query: {
+      fetchPolicy: "no-cache",
+    },
+  },
+});
+
+export { blogUpdatingClient };
 export default client;
