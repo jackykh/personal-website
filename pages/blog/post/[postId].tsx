fa63df54de9cf02ccf422859c62f43c8ff29d5fb
Jacky Cheung <cheungkwingho@gmail.com>
2025-01-01 06:04:43 +0800

    adjust ai summary text box style

--- pages/blog/post/[postId].tsx
+++ pages/blog/post/[postId].tsx
@@ -110,7 +110,7 @@ const Post = (props: postProps) => {
                           cy="12"
                           r="10"
                           stroke="currentColor"
-                          stroke-width="4"
+                          strokeWidth="4"
                         ></circle>
                         <path
                           className="opacity-75"
